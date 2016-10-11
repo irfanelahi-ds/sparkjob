1bfa96ae6e90b4ed8c957aef14439a6e4669d540
d814394 <irfan.elahi@team.telstra.com>
2016-10-11 16:45:27 +1100

    changed example code

---
+++ src/main/scala/com/examples/MainExample.scala
@@ -0,0 +1,16 @@
+package com.examples
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+import org.apache.log4j.Logger
+
+object MainExample {
+
+  def main(arg: Array[String]) {
+
+    val x1="hello"
+	println(x1.length)
+
+  }
+}
